8f314e2455c5ed236542494fd6591d48f4968b6c
Daniel Krastev <danielkrastev01@gmail.com>
2025-01-09 11:12:11 +0200

    Created load assets and routings

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 import http from 'http';
 
+import homePage from './views/home/index.html.js';
+import addBreed from './views/addBreed.html.js';
+import addCat from './views/addCat.html.js';
+import siteCss from './content/styles/site.css.js';
+
 const server = http.createServer((req, res) => {
-    console.log('It works');
+    // Load assets
+    if (req.url === '/styles/site.css') {
+        res.writeHead(200, {
+            'Content-type': 'text/css',
+        })
+
+        res.write(siteCss);
+        return res.end();
+    }
+
+    res.writeHead(200, {
+        'Content-type': 'text/html',
+    })
+
+    // Routings
+    switch(req.url){
+        case '/': 
+            res.write(homePage);
+            break;
+        case '/cats/add-breed': 
+            res.write(addBreed);
+            break;
+        case '/cats/add-cat': 
+            res.write(addCat);
+            break;
+        default:
+            res.write('Page not found');
+            break;
+    }
+    
 
     res.end();
     
