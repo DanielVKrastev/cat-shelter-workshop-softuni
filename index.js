ffb532b10d8ab4f0241671379d162a671b6bf5e6
Daniel Krastev <danielkrastev01@gmail.com>
2025-01-09 11:21:44 +0200

    Dynamic generated cats

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 import http from 'http';
 
 import homePage from './views/home/index.html.js';
-import addBreed from './views/addBreed.html.js';
-import addCat from './views/addCat.html.js';
+import addBreedPage from './views/addBreed.html.js';
+import addCatPage from './views/addCat.html.js';
 import siteCss from './content/styles/site.css.js';
+
+const cats = [
+    {
+        id: 1,
+        imageUrl: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=',
+        name: 'Pretty Kitty',
+        breed: 'Bombay Cat',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+    },
+    {
+        id: 2,
+        imageUrl: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=',
+        name: 'Navcho',
+        breed: 'Persian Cat',
+        description: 'A talkative and affectionate cat with striking yellow eyes.',
+    },
+    {
+        id: 3,
+        imageUrl: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=',
+        name: 'Sisa',
+        breed: 'Siamese Cat',
+        description: 'Loves to cuddle and nap. Requires regular grooming for its luxurious coat.',
+    },
+    {
+        id: 4,
+        imageUrl: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=',
+        name: 'Garry',
+        breed: 'Bombay Cat',
+        description: 'Mysterious and elegant. Often found lounging in sunny spots.',
+    },
+];
 
 const server = http.createServer((req, res) => {
     // Load assets
@@ -23,13 +54,13 @@ const server = http.createServer((req, res) => {
     // Routings
     switch(req.url){
         case '/': 
-            res.write(homePage);
+            res.write(homePage(cats));
             break;
         case '/cats/add-breed': 
-            res.write(addBreed);
+            res.write(addBreedPage());
             break;
         case '/cats/add-cat': 
-            res.write(addCat);
+            res.write(addCatPage());
             break;
         default:
             res.write('Page not found');
